ea047e00e535aa99187c76cc9f6d0925b74c348e
Daniyar <kaliyev.daniyar@gmail.com>
2016-11-20 17:01:24 +0000

    First commit

---
+++ IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IRepository<T, K>
+    {
+        IQueryable<T> AsQueryable();
+
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        T GetById(K id);
+
+        void Create(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+    }
+}
---
+++ IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        IRepository<T,K> GetRepository<T, K>() where T: class, IEntity<K>;
+        void Commit();
+        void Rollback();
+    }
+}
